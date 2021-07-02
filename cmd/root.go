75c4d7dbb8f91fef2fa9860a300a44cedfa9a8ba
Jasmine Mai <Jasmine.W.Mai.20@dartmouth.edu>
2021-07-02 11:42:52 -0700

    add makefile, copyright

--- cmd/root.go
+++ cmd/root.go
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2021 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package main
 
 import (
@@ -13,8 +30,8 @@ var (
 	useRebootParser bool
 
 	rootCmd = &cobra.Command{
-		Use:   "cmd",
-		Short: "cmd gets, parses, and validates a list of events",
+		Use:   "interpreter",
+		Short: "interpreter gets, parses, and validates a list of events",
 	}
 )
 
