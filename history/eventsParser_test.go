6d5b2ea225713146ec8fa52795f4b3ce49c22c1c
Jasmine Mai <Jasmine.W.Mai.20@dartmouth.edu>
2021-07-15 18:43:03 -0700

    add unit tests for new helper functions

--- history/eventsParser_test.go
+++ history/eventsParser_test.go
@@ -689,6 +689,202 @@ func TestRebootEndParser(t *testing.T) {
 	}
 }
 
+func TestGetSameBootTimeEvents(t *testing.T) {
+	now, err := time.Parse(time.RFC3339Nano, "2021-03-02T18:00:01Z")
+	assert.Nil(t, err)
+	testErr := errors.New("test")
+	events := []interpreter.Event{
+		interpreter.Event{
+			Metadata: map[string]string{
+				interpreter.BootTimeKey: fmt.Sprint(now.Unix()),
+			},
+			TransactionUUID: "1",
+		},
+		interpreter.Event{
+			Metadata: map[string]string{
+				interpreter.BootTimeKey: fmt.Sprint(now.Unix()),
+			},
+			TransactionUUID: "2",
+		},
+		interpreter.Event{
+			Metadata: map[string]string{
+				interpreter.BootTimeKey: fmt.Sprint(now.Add(time.Minute).Unix()),
+			},
+			TransactionUUID: "4",
+		},
+		interpreter.Event{
+			Metadata: map[string]string{
+				interpreter.BootTimeKey: fmt.Sprint(now.Add(3 * time.Minute).Unix()),
+			},
+			TransactionUUID: "5",
+		},
+		interpreter.Event{
+			Metadata: map[string]string{
+				interpreter.BootTimeKey: fmt.Sprint(now.Unix()),
+			},
+			TransactionUUID: "3",
+		},
+		interpreter.Event{
+			Metadata: map[string]string{
+				interpreter.BootTimeKey: fmt.Sprint(now.Add(-1 * time.Minute).Unix()),
+			},
+			TransactionUUID: "6",
+		},
+	}
+
+	invalidComparator := new(mockComparator)
+	validComparator := new(mockComparator)
+	invalidComparator.On("Compare", mock.Anything, mock.Anything).Return(true, testErr)
+	validComparator.On("Compare", mock.Anything, mock.Anything).Return(false, nil)
+
+	tests := []struct {
+		description string
+		event       interpreter.Event
+		comparator  Comparator
+		expectedErr error
+	}{
+		{
+			description: "no current boot-time",
+			event:       interpreter.Event{},
+			expectedErr: interpreter.ErrBootTimeNotFound,
+			comparator:  validComparator,
+		},
+		{
+			description: "success",
+			event: interpreter.Event{
+				Metadata: map[string]string{
+					interpreter.BootTimeKey: fmt.Sprint(now.Unix()),
+				},
+			},
+			comparator: validComparator,
+		},
+		{
+			description: "invalid comparator",
+			event: interpreter.Event{
+				Metadata: map[string]string{
+					interpreter.BootTimeKey: fmt.Sprint(now.Unix()),
+				},
+			},
+			comparator:  invalidComparator,
+			expectedErr: testErr,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			assert := assert.New(t)
+
+			currentBootTime, _ := tc.event.BootTime()
+			var expectedEvents []interpreter.Event
+			expectedEvents = append(expectedEvents, tc.event)
+			for _, event := range events {
+				eventBootTime, _ := event.BootTime()
+				if currentBootTime == eventBootTime {
+					expectedEvents = append(expectedEvents, event)
+				}
+			}
+
+			results, err := getSameBootTimeEvents(events, tc.event, tc.comparator)
+
+			if tc.expectedErr == nil {
+				assert.ElementsMatch(expectedEvents, results)
+				assert.Equal(tc.expectedErr, err)
+			} else {
+				assert.True(errors.Is(err, tc.expectedErr))
+			}
+		})
+	}
+}
+func TestSameEvent(t *testing.T) {
+	now, err := time.Parse(time.RFC3339Nano, "2021-03-02T18:00:01Z")
+	assert.Nil(t, err)
+	tests := []struct {
+		description string
+		eventA      interpreter.Event
+		eventB      interpreter.Event
+		sameEvent   bool
+	}{
+		{
+			description: "same event",
+			eventA: interpreter.Event{
+				TransactionUUID: "123",
+				Metadata: map[string]string{
+					interpreter.BootTimeKey: fmt.Sprint(now.Unix()),
+				},
+				Birthdate: now.UnixNano(),
+			},
+			eventB: interpreter.Event{
+				TransactionUUID: "123",
+				Metadata: map[string]string{
+					interpreter.BootTimeKey: fmt.Sprint(now.Unix()),
+				},
+				Birthdate: now.UnixNano(),
+			},
+			sameEvent: true,
+		},
+		{
+			description: "different uuid",
+			eventA: interpreter.Event{
+				TransactionUUID: "123",
+				Metadata: map[string]string{
+					interpreter.BootTimeKey: fmt.Sprint(now.Unix()),
+				},
+				Birthdate: now.UnixNano(),
+			},
+			eventB: interpreter.Event{
+				TransactionUUID: "abc",
+				Metadata: map[string]string{
+					interpreter.BootTimeKey: fmt.Sprint(now.Unix()),
+				},
+				Birthdate: now.UnixNano(),
+			},
+			sameEvent: false,
+		},
+		{
+			description: "different boot-times",
+			eventA: interpreter.Event{
+				TransactionUUID: "123",
+				Metadata: map[string]string{
+					interpreter.BootTimeKey: fmt.Sprint(now.Unix()),
+				},
+				Birthdate: now.UnixNano(),
+			},
+			eventB: interpreter.Event{
+				TransactionUUID: "123",
+				Metadata: map[string]string{
+					interpreter.BootTimeKey: fmt.Sprint(now.Add(time.Minute).Unix()),
+				},
+				Birthdate: now.UnixNano(),
+			},
+			sameEvent: false,
+		},
+		{
+			description: "different birthdates",
+			eventA: interpreter.Event{
+				TransactionUUID: "123",
+				Metadata: map[string]string{
+					interpreter.BootTimeKey: fmt.Sprint(now.Unix()),
+				},
+				Birthdate: now.UnixNano(),
+			},
+			eventB: interpreter.Event{
+				TransactionUUID: "123",
+				Metadata: map[string]string{
+					interpreter.BootTimeKey: fmt.Sprint(now.Unix()),
+				},
+				Birthdate: now.Add(time.Minute).UnixNano(),
+			},
+			sameEvent: false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			assert.Equal(t, tc.sameEvent, sameEvent(tc.eventA, tc.eventB))
+		})
+	}
+}
+
 func TestSetComparator(t *testing.T) {
 	assert := assert.New(t)
 	comparator := setComparator(nil)
